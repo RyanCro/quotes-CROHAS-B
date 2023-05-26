76d4790c21da0e2e24bb54b0dab2a8414d06d1c2
Hadile <hadile.kerrit@efrei.net>
2023-05-26 08:45:08 +0200

    Ajout de display quotes

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ def menu():
     print("1. Random quote")
     print("2. All quotes")
     print("3. Exit")
+    print("5. Display quotes")
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-3): ")
+        choice = input("Choose your an action (1-5): ")
 
         if choice == "1":
             print_quote(random_quote(quotes))
@@ -20,6 +21,9 @@ def main():
         elif choice == "3":
             print("Good bye...")
             break
+        elif choice == "5": # gestion de display_count()
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
         else:
             print("Invalid input")
 
